564e6263a55d3f212bab0426a8c728cfd163ab82
isaac <isaactuckey@knights.ucf.edu>
2023-04-07 18:30:09 -0400

    translated but broken

---
+++ Weakheap.java
@@ -0,0 +1,87 @@
+// Isaac Tuckey (4996772)
+// COT-5402, Spring 23
+
+import java.util.Arrays;
+
+public class Weakheap {
+    // h is the 1-d array used to implement the weak heap
+    private int[] h;
+    // reverse[i] is the 'bit' needed to determine i's right and left child
+    private boolean[] reverse;
+
+    // Class initializer
+    public Weakheap(int[] inputArray) {
+        final int n = inputArray.length;
+        this.reverse = new boolean[n+1];
+        // We need to copy the input with an extra space to sort in place
+        this.h = new int[n+1];
+        for (int i = 0; i < n; i++) {
+            this.h[i] = inputArray[i];
+        }
+    }
+
+    // Returns the index of j's grandparent
+    private int getGrandparent(int j) {
+        // while: odd(j) = reverse(j/2)
+        while ((j % 2 != 0) == this.reverse[j/2]) {
+            j = j/2;
+        }
+        return j/2;
+    }
+
+    // Swaps values in h[i] and h[j]
+    private void swap(int i, int j) {
+        final int temp = this.h[i];
+        this.h[i] = this.h[j];
+        this.h[j] = temp;
+    }
+
+    // Merges two sub-weak-heaps with roots at i and j
+    private void merge(int i, int j) {
+        if (this.h[i] < this.h[j]) {
+            swap(i, j);
+            this.reverse[j] = !this.reverse[j];
+        }
+    }
+
+    private void weakHeapify(int n) {
+        for (int j = n-1; j > 0; j--) {
+            merge(getGrandparent(j), j);
+        }
+    }
+
+    private void mergeForest(int m) {
+        int x = 1;
+        this.reverse[m/2] = false;
+        if (m >= 3) {
+            while (2 * x + (this.reverse[x] ? 1 : 0) >= m) {
+                x = 2*x + (this.reverse[x] ? 1 : 0);
+            }
+            while (x > 0) {
+                merge(m, x);
+                x = x/2;
+            }
+        }
+    }
+
+    public void weakHeapSort(int n) {
+        weakHeapify(n);
+        this.h[n] = this.h[0];
+        for (int i = n-1; i > 1; i--) {
+            mergeForest(i);
+        }
+    }
+
+    public void printSortedArray() {
+        System.out.println(Arrays.toString(this.h));
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray = new int[] { 9, 3, 4, 32, 27, 7, 12, 20, 6 };
+        final int n = inputArray.length;
+        Weakheap heap = new Weakheap(inputArray);
+        heap.weakHeapSort(n);
+        heap.printSortedArray();
+    }
+
+}
--- weakheap.c
+++ weakheap.c
@@ -1,9 +1,35 @@
 // Isaac Tuckey (4996772)
 // COT-5402, Spring 23
 
-#include <stdio.h>
+
+
+int Gparent(int j, int* reverse) {
+    while (odd(j) == reverse[j/2]) {
+        j = j/2;
+    }
+    return j/2;
+}
+
+void Merge (int* i, int* j) {
+
+}
+
+weakheap * weakHeapify(int n, int* arr) {
+    // Initialize arrays and allocate memory
+    weakheap * ret = malloc(sizeof(weakheap));
+    ret->h = calloc(n+1, sizeof(int));
+    ret->reverse = calloc(n+1, sizeof(int));
+
+
+
+
+}
+
+void weakHeapSort(int n, int *h) {
+    weakHeapify(n, h);
+}
 
 int main(void) {
-    puts("Hello, World!");
     return 0;
 }
+
